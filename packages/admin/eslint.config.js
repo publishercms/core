50f09a0068822f23e6fd036da7c9b947ad2553e3
Andreas Ekström <didair@msn.com>
2025-08-28 00:01:07 +0200

    Remove default files, add dependencies

--- packages/admin/eslint.config.js
+++ packages/admin/eslint.config.js
@@ -19,5 +19,10 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    overrides: {
+      rules: {
+        "react-refresh/only-export-components": "off",
+      },
+    },
   },
 ])
--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite"
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/admin/",
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    // Add vite plugins below this line:
+    tsConfigPaths(),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
