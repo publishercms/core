17ae7a66316710c62851806a6ab52380a5b69aa4
Andreas Ekström <didair@msn.com>
2025-08-29 00:25:05 +0200

    Update client sdk to export new user api for login etc

--- packages/client/index.d.ts
+++ packages/client/index.d.ts
@@ -1,7 +1,15 @@
 import type { TRPCClient } from "@trpc/client";
 import type { AnyRouter } from "@trpc/server";
 
-export type PublisherClient<TRouter extends AnyRouter = AnyRouter> = TRPCClient<TRouter>;
+export type PublisherClient<TRouter extends AnyRouter = AnyRouter> = {
+  trpc: TRPCClient<TRouter>
+  user: {
+    login: (values: {
+      email: string;
+      password: string;
+    }) => Promise<string | undefined>;
+  },
+};
 
 export declare function createClient<TRouter extends AnyRouter = AnyRouter>(config: {
   apiUrl: string;
--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,11 +1,58 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
+function setAuthToken(token: string) {
+  localStorage.setItem('pubcms-token', token);
+};
+
+function getAuthToken() {
+  return localStorage.getItem('pubcms-token');
+};
+
 export function createClient(config: { apiUrl: string }) {
-  return createTRPCProxyClient<any>({
+  const trpc = createTRPCProxyClient<any>({
     links: [
       httpBatchLink({
         url: config.apiUrl + "/trpc",
+        headers() {
+          const token = getAuthToken();
+          if (token) {
+            return {
+              'Authorization': `Bearer ${token}`,
+            };
+          }
+
+          return {};
+        }
       }),
     ],
   });
+
+  const user = {
+    login: async ({ email, password}: {
+      email: string;
+      password: string;
+    }) => {
+      const response: {
+        token: string;
+        user: {
+          id: string;
+          email: string;
+          name: string;
+          role: string;
+        },
+      } = await (trpc as any).authentication.login.mutate({ email, password });
+
+      if (response != null && response.token != null) {
+        setAuthToken(response.token);
+        return response.token;
+      }
+
+      return undefined;
+    },
+  };
+
+  return {
+    trpc,
+    user,
+  };
 };
